f77729051d7b4f05e9ce1b079c9e324be63c8424
sujit70614 <sujitsingh70614@gmail.com>
2025-12-01 22:05:05 +0530

    Implement a simple calculator in Python

---
+++ Calculator.py
@@ -0,0 +1,26 @@
+print("Simple Calculator")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+print("5. Exit")
+choice = input("Enter your choice (1-5): ")
+if choice in ["1", "2", "3", "4"]:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    if choice == "1":
+        print("Result:", num1 + num2)
+    elif choice == "2":
+        print("Result:", num1 - num2)
+    elif choice == "3":
+        print("Result:", num1 * num2)
+    elif choice == "4":
+        if num2 != 0:
+            print("Result:", num1 / num2)
+        else:
+            print("Error: Cannot divide by zero")
+elif choice == "5":
+        print("You are done.")
+        exit()
+else:
+    print("Invalid choice")
